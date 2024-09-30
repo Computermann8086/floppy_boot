c5aeafcfc68d2d0d47778b70f24fe8d1a428fcf6
Computer Mann <112242931+Computermann8086@users.noreply.github.com>
2024-09-30 09:21:32 +0200

    Update bootsector.asm

--- bootsector.asm
+++ bootsector.asm
@@ -14,6 +14,17 @@
 org 7c00h               ; Program origin of 7c00h
 bits 16                 ; 16-bit programing
 cpu 8086                ; And assembling for the iAPX 8086 processor
+jmp start
+nop
+BPB:
+bps  dw 512             ; Bytes Per Sector
+spc  db 2               ; Sectors Per Cluster
+rs   dw 0               ; Reserved Sectors
+
+
+start:
+jmp 0000:enforce_csip   ; Preforming a far jump to enforce CS:IP
+enforce_csip:           ; Now that CS:IP is enforced
 cli                     ; Clear the interrupt flag, only non-maskable interrupts will be enabled
 xor ax, ax              ; Zero out accumulator
 mov ss, ax              ; Set the stack segment register
@@ -23,7 +34,6 @@ push ax                 ; Pushing AX again
 pop es                  ; And popping it into ES
 pop ds                  ; and DS and setting them both to zero
 sti                     ; Set the interrupts flag back on, now every single interrupt can interrupt my work
-jmp 0000:enforce_csip   ; Preforming a far jump to enforce CS:IP
-enforce_csip:
+
 
 
