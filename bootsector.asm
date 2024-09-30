4089341f6238c907a28fe85fd281058ce74b5e7a
Computer Mann <112242931+Computermann8086@users.noreply.github.com>
2024-09-30 09:06:31 +0200

    Update bootsector.asm

--- bootsector.asm
+++ bootsector.asm
@@ -1,3 +1,26 @@
-org 7c00h
-bits 16
-cpu 8086
+;----------------------------------;
+;        Floppy Boot Record        ;
+;  Written by Christian Henrichsen ;
+;            30.09.2024            ;
+;           Copyright (C)          ;
+;       Christian Henrichsen       ;
+;               2024               ;
+;                                  ;
+;     Copyright Notice Must be     ;
+; Included with All Copies of This ;
+;              Program             ;
+;----------------------------------;
+
+org 7c00h               ; Program origin of 7c00h
+bits 16                 ; 16-bit programing
+cpu 8086                ; And assembling for the iAPX 8086 processor
+cli                     ; Clear the interrupt flag, only non-maskable interrupts will be enabled
+xor ax, ax              ; Zero out accumulator
+mov ss, ax              ; Set the stack segment register
+mov sp, 7000h           ; and the Stack Pointer
+push ax                 ; Pushing AX 
+push ax                 ; Pushing AX again
+pop es                  ; And popping it into ES
+pop ds                  ; and DS and setting them both to zero
+sti                     ; Set the interrupts flag back on, now every single interrupt can interrupt my work
+
